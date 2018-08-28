80fe432765217483e3bf1a342cc682e5fd734422
Sunshine Daydream <alexbruner@live.com>
2018-08-28 18:24:40 -0500

    login page built

--- client/src/pages/CreateUserPage.jsx
+++ client/src/pages/CreateUserPage.jsx
@@ -19,33 +19,33 @@ class CreateUserPage extends Component {
     errorMsg:''
    }
 
-   handleSubmit = (event) => {
-    event.preventDefault();
+  handleSubmit = (event) => {
+  event.preventDefault();
 
-    // Check Passwords are the same
-    if (this.state.password === this.state.password_v){
-      return true;
-    }
-    else{
-      console.log(this.state.password);
-      console.log(this.state.password_v);
-      return false;
-    }
-    let newUserInput = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password
-    }
-    console.log(newUserInput)
-    
-    axios.post('/api/user/new', newUserInput)
-      .then(function(response){
-        console.log(response)
-      })
-      .catch(function(error){
-        console.log(error);
-      })
-   }
+  // Check Passwords are the same
+  // if (this.state.password === this.state.password_v){
+  //   return true;
+  // }
+  // else{
+  //   console.log(this.state.password);
+  //   console.log(this.state.password_v);
+  //   return false;
+  // }
+  let data = {
+    username: this.state.name,
+    email: this.state.email,
+    password: this.state.password
+  }
+  console.log(data)
+  
+  axios.post('http://localhost:3001/api/user/new', data)
+    .then(function(response){
+      console.log(response)
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  }
 
   // Render to Screen
   render() { 
@@ -56,52 +56,50 @@ class CreateUserPage extends Component {
           subpage = {this.state.subpage}
         />
 
-          <form className='container'>
-            <p className='m-0 p-0 text-danger'>{this.state.errorMsg}</p>
-            <input 
-              className='m-2'
-              name='fullname'
-              placeholder='Full Name'
-              type='text' 
-              value={this.state.name}
-              onChange={e => this.setState({ name: e.target.value})}
-            />
-            <input
-              className='m-2'
-              name='email'
-              placeholder='Email Address'
-              type="text"
-              value={this.state.email}
-              onChange={e => this.setState({ email: e.target.value})}
-            />
-            <input
-              className='m-2'
-              name='password'
-              placeholder='Passcode'
-              type="password"
-              value={this.state.password}
-              onChange={e => this.setState({ password: e.target.value})}
-            />
-            <input
-              className='m-2'
-              name='password_v'
-              placeholder='Retype Passcode'
-              type="password"
-              value={this.state.password_v}
-              onChange={e => this.setState({ password_v: e.target.value})}
-            />
-            <br/>
-            <button
-              className='btn btn-info btn-small'
-              onClick={this.handleSubmit}
-            >
-              Submit
-            </button>
+        <form className='container'>
+          <p className='m-0 p-0 text-danger'>{this.state.errorMsg}</p>
+          <input 
+            className='m-2'
+            name='fullname'
+            placeholder='Full Name'
+            type='text' 
+            value={this.state.name}
+            onChange={e => this.setState({ name: e.target.value})}
+          />
+          <input
+            className='m-2'
+            name='email'
+            placeholder='Email Address'
+            type="text"
+            value={this.state.email}
+            onChange={e => this.setState({ email: e.target.value})}
+          />
+          <input
+            className='m-2'
+            name='password'
+            placeholder='Passcode'
+            type="password"
+            value={this.state.password}
+            onChange={e => this.setState({ password: e.target.value})}
+          />
+          <input
+            className='m-2'
+            name='password_v'
+            placeholder='Retype Passcode'
+            type="password"
+            value={this.state.password_v}
+            onChange={e => this.setState({ password_v: e.target.value})}
+          />
+          <br/>
+          <button
+            className='m-2 btn btn-info btn-small'
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </button>
+        </form>
 
-
-          </form>
-        <Footer
-        />
+        <Footer/>
       </div>
 
     );
