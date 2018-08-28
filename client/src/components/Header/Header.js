198893009947411b51c2b8e204e1c9c0a0e74aee
Sunshine Daydream <alexbruner@live.com>
2018-08-27 20:00:57 -0500

    adjusted for create-react-app

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+const Header = props => (
+
+  <div className="bg-green text-light border-bottom border-dark p-3">
+    <h1 className="title text-center font-weight-bold">{props.title}</h1>
+    <p className='text-info'>- {props.subpage}</p>
+
+  </div>
+);
+
+export default Header;
