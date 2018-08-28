60b83b82f27dc90ec04ed766286bc0ec9e18c639
Sunshine Daydream <alexbruner@live.com>
2018-08-27 22:22:37 -0500

    working on it - react

---
+++ client/src/components/CreateUserForm/createuserform.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import {Checkbox, Radio, FormGroup, ControlLabel, FormControl, Button, HelpBlock } from 'react-bootstrap'
+
+function FieldGroup({id, label, help, ...props}){
+  return (
+    <FormGroup controlId={id}>
+    <ControlLabel>{label}</ControlLabel>
+    <FormControl {...props} />
+    {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
+
+
+const CreateUserForm = props => (
+  
+  <form>
+  <label htmlFor="username">Enter username</label>
+  <input id="username" name="username" type="text" />
+
+  <label htmlFor="email">Enter your email</label>
+  <input id="email" name="email" type="email" />
+
+  <label htmlFor="birthdate">Enter your birth date</label>
+  <input id="birthdate" name="birthdate" type="text" />
+
+  <button onClick = {props.handleSubmit}>Send data!</button>
+</form>
+
+    
+
+);
+
+export default CreateUserForm;
