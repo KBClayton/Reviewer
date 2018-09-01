679137fb5a690b0d6e5bdec96b02814782fbc6d4
KBClayton <clayton.kevinb@gmail.com>
2018-09-01 17:47:30 -0500

    I hate merging

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
 // import { BrowserRouter, Route, Link } from 'react-router-dom'
-<<<<<<< HEAD
-=======
-import { BrowserRouter, Route, Link } from 'react-router-dom'
->>>>>>> 198893009947411b51c2b8e204e1c9c0a0e74aee
-=======
 import './Header.css';
->>>>>>> merge
 
 const Header = props => (
 
