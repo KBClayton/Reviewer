3bbd47097aed7cc319360817c3a4b45e2e440fa7
KBClayton <clayton.kevinb@gmail.com>
2018-09-01 19:14:47 -0500

    this stuff?

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Animations CSS Library
 import 'animate.css'
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
 
