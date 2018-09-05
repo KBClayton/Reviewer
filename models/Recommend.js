835a6d5437241990eaf0e96ab5a02be59c546f2d
jamie reaves <jamiereaves@utexas.edu>
2018-09-05 17:07:48 -0500

    scraping routes are mostly done. need to add a music event and arts event route for the chronicle.

--- models/Recommend.js
+++ models/Recommend.js
@@ -2,15 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recommendSchema = new Schema ({
-  title: {type: String, required: true},
+  title: { type: String, required: true},
   description: { type: String},
-  location: {type: String},
-  time: {type: String},
-  image:{type:String, default:"https://via.placeholder.com/300x300"},
-  link:{ type: String, default: "https://via.placeholder.com" },
-  do512: {type: Boolean, required: true, default: false},
-  food: {type: Boolean, required:true, default: false},
-  music: {type: Boolean, required: true, default: false}
+  location: { type: String},
+  address: { type: String},
+  time: { type: String},
+  image: { type:String, default: "https://via.placeholder.com/300x300"},
+  link: { type: String, default: "https://via.placeholder.com" },
+  ticketLink: { type: String, default: "" },
+  lat: { type: String, default: "" },
+  long: { type: String, default: "" },
+  do512: { type: Boolean, required: true, default: false},
+  food: { type: Boolean, required: true, default: false},
+  music: { type: Boolean, required: true, default: false},
+  books: { type: Boolean, required: true, default: false },
+  obscura: { type: Boolean, required: true, default: false},
+  outdoor: { type: Boolean, required: true, default: false},
+  artsEvent: { type: Boolean, required: true, default: false},
 })
 
 const Recommend = mongoose.model("Recommend", recommendSchema);
