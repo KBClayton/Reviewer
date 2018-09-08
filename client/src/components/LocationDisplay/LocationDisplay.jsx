de208fb8f9461fc2ccb50a4e9348572d61b1a41d
Sunshine Daydream <alexbruner@live.com>
2018-09-08 10:41:03 -0500

    banana

--- client/src/components/LocationDisplay/LocationDisplay.jsx
+++ client/src/components/LocationDisplay/LocationDisplay.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom'
 const LocationDisplay = props => (
   <div>
     <div className = 'card p-3 m-3'>
-      <a href={props.link}><h5><b>{props.title}</b></h5></a>
+      <a href={`//${props.link}`} target='_blank'><h5><b>{props.title}</b></h5></a>
       <p>{props.description}</p>
       {/* <a href='localhost:3001/api/articles'><button className = 'btn-danger'>Save</button></a> */}
       <Link to={props.urlLink}><button className = "btn btn-info m-3" >Comments</button></Link>
